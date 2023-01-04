62f442459c8c9d8f7314c99b5594bc89ab8da916
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-01-04 04:46:35 -0300

    43. Criando Widget Questão

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 void main() {
   runApp(const PerguntaApp());
@@ -32,7 +33,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: [
-            Text(perguntas[_perguntaSelecionada]),
+            Questao(perguntas[_perguntaSelecionada]),
             ElevatedButton(
                 onPressed: _responder, child: const Text('Resposta 1')),
             ElevatedButton(
