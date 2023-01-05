92e6d3e3c975a94c290b4327d86bd38c27c38053
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-01-05 05:46:02 -0300

    55. Evitando Acessar Pergunta Inválida

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,82 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
 import './questao.dart';
 import './resposta.dart';
 
-void main() {
-  runApp(const PerguntaApp());
-}
+void main() => runApp(const PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Lilás', 'Marron', 'Verde', 'Amarelo']
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Cachorro', "Gato", "Leão", "Papagaio"]
+    },
+    {
+      'texto': "Qual é o seu time favorito?",
+      "respostas": ['Cruzeiro', 'Real Madrid', 'Borussia Dortimund', "Milan"]
+    },
+    {
+      'texto': "Qual é o seu melhor amigo?",
+      'respostas': ['Gerson', 'Fábio', 'Alessando', 'Jeremias']
+    },
+    {
+      'texto': "Qual é a sua melhor amiga?",
+      'respostas': ['Elizabeth', 'Girlene', 'Nilza', 'Leila']
+    },
+    {
+      'texto': "Qual esporte favorito?",
+      'respostas': ['Futebol', 'Volei', "Handebol", 'Basquete']
+    },
+    {
+      'texto': "Qual a sua primeira liguagem programação?",
+      'respostas': ["C#", "Javascript", "Python", 'Dart']
+    },
+    {
+      'texto': "Qual é o seu melhor instrutor",
+      'respostas': [
+        "Leonardo Leitão",
+        "Danilo Aparecido",
+        "Daniel Tapias Morales",
+        'Glauco Daniel e João Rangel'
+      ]
+    }
+  ];
 
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      {
-        'texto': 'Qual é a sua cor favorita?',
-        'respostas': ['Lilás', 'Marron', 'Verde', 'Amarelo']
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': ['Cachorro', "Gato", "Leão", "Papagaio"]
-      },
-      {
-        'texto': "Qual é o seu time favorito?",
-        "respostas": ['Cruzeiro', 'Real Madrid', 'Borussia Dortimund', "Milan"]
-      },
-      {
-        'texto': "Qual é o seu melhor amigo?",
-        'respostas': ['Gerson', 'Fábio', 'Alessando', 'Jeremias']
-      },
-      {
-        'texto': "Qual é a sua melhor amiga?",
-        'respostas': ['Elizabeth', 'Girlene', 'Nilza', 'Leila']
-      },
-      {
-        'texto': "Qual esporte favorito?",
-        'respostas': ['Futebol', 'Volei', "Handebol", 'Basquete']
-      },
-      {
-        'texto': "Qual a sua primeira liguagem programação?",
-        'respostas': ["C#", "Javascript", "Python", 'Dart']
-      },
-      {
-        'texto': "Qual é o seu melhor instrutor",
-        'respostas': [
-          "Leonardo Leitão",
-          "Danilo Aparecido",
-          "Daniel Tapias Morales",
-          'Glauco Daniel e João Rangel'
-        ]
-      }
-    ];
-
-    List respostas = perguntas[_perguntaSelecionada]['respostas'] as List;
-    // List<Widget> widget = respostas.map((t) => Resposta(t, _responder)).toList();
-
-    // for (String textoRep in respostas ) {
-    //   widget.add(Resposta(textoRep, _responder));
-    // }
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada]['respostas'] as List<String>
+        : [];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
-        body: Column(
-          children: [
-            Questao(perguntas[_perguntaSelecionada]['texto'] as String),
-            ...respostas.map((t) => Resposta(t, _responder)).toList(),
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Column(
+                children: [
+                  Questao(_perguntas[_perguntaSelecionada]['texto'] as String),
+                  ...respostas.map((t) => Resposta(t, _responder)).toList(),
+                ],
+              )
+            : null,
       ),
     );
   }
